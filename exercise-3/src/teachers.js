e0b0160f57ff699348c233372813c7957ad39d0a
HEM ROTHAPANHARITH <GMRithzz123@gmail.com>
2026-02-12 14:51:59 +0700

    Completed Ex3

--- exercise-3/src/teachers.js
+++ exercise-3/src/teachers.js
@@ -7,16 +7,16 @@ export const ALL_PNV_TEACHERS = [
   {
     firstName: "sd",
     lastName: "Ngo",
-    title: "IT Trainer",
+    title: "Trainer",
   },
   {
     firstName: "sadad",
     lastName: "Ngo",
-    title: "IT Trainer",
+    title: "Java Trainer",
   },
   {
     firstName: "asdadsad",
     lastName: "Ngo",
-    title: "IT Trainer",
+    title: "C++ Trainer",
   },
 ];
